c1cb081830e0526b3665305f5a675132afbc9ca1
Dear Martinez <dearmartinez@icloud.com>
2015-07-06 19:47:29 -0400

    Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,11 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+/* GET Créditos page. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Créditos' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
