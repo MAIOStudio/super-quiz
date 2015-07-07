510fae814eb4626706d7c69d778c5a4753f6cba6
Dear Martinez <dearmartinez@icloud.com>
2015-07-07 11:06:58 -0400

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -34,14 +34,16 @@ exports.Quiz = Quiz; // Exportar definición de tabla Quiz
 
 // sequielize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
-	// success(..) ejecuta el manejador una vez creada la tabla
+	// then(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function (count){
 		if(count === 0){ // La tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: '¿Capital de Italia?',
 						  respuesta: 'Roma'
-
+			});
+	Quiz.create({ pregunta: '¿Capital de Portugal?',
+						  respuesta: 'Lisboa'
 			})
-		 .success(function(){console.log('Base de datos inicializada')});
+	.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 }) ;
