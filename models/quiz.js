86b376ff822d808db496d2fb6f5da1214f3dd03d
Dear Martinez <dearmartinez@icloud.com>
2015-07-07 13:42:04 -0400

    Autoload de la DB

--- models/quiz.js
+++ models/quiz.js
@@ -1,15 +1,8 @@
 // Definicion del modelo Quiz
 
-<<<<<<< HEAD
 module.exports = function(sequelize, DataTypes){
 	return sequelize.define('Quiz',
 				{pregunta: DataTypes.STRING,
 				 respuesta: DataTypes.STRING,
-=======
-module.exports = function(sequelize, Datatypes){
-	return sequelize.define('Quiz',
-				{pregunta: Datatypes.STRING,
-				 respuesta: Datatypes.STRING,
->>>>>>> 86e45a7de2b8df3490aebbe2e6930e03e648ff78
 				});
 }
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
 /* GET Créditos page. */
 router.get('/author', function(req, res) {
   res.render('author', { title: 'Créditos' });
 });
 
 // Definición de rutas de /quizes
-router.get('quizes', quizController.index);
+router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
