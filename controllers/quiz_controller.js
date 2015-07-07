1cdfd7468a712171f10c987453e9013a7dfd9926
Dear Martinez <dearmartinez@icloud.com>
2015-07-07 18:34:22 -0400

    Cuadro de Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,23 +2,34 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
-	models.Quiz.find(quizId).then(
+	models.Quiz.findById(quizId).then(
 		function(quiz){
 			if(quiz){
 				req.quiz = quiz;
 				next();
-			} else { next(new Error('No existe el quizId=' quizId));}
+			} else { next(new Error('No existe el quizId=' + quizId));}
 		}
 	).catch(function(error) {next(error);});
 };
 
 // GET quizes
 exports.index = function(req, res){
-	models.Quiz.findAll().then(
-		function(quizes){
-			res.render('quizes/index', {quizes: quizes});
-		}
-	).catch(function(error) {next(error);})
+	var filtro = req.query.search;
+		var condicion = ('%' + filtro + '%').replace(/ /g,'%');
+	if(req.query.search){
+		models.Quiz.findAll({
+			where: ["pregunta like ?", condicion],
+			order: [['pregunta', 'ASC']]}
+			).then(function(quizes){
+				res.render('quizes/index', {quizes: quizes});
+			}).catch(function(error) {next(error);});
+	} else {
+		models.Quiz.findAll().then(
+			function(quizes){
+				res.render('quizes/index', {quizes: quizes});
+			}
+		).catch(function(error) {next(error);})
+	}
 };
 
 // GET /quizes/:id
